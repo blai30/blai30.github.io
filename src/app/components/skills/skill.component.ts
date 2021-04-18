914634fc49ca57b30ccb9a6ecbe11c5894829eeb
Brian Lai <brianlai30@outlook.com>
2021-04-18 15:27:50 -0700

    feat: add logos and filled out skills

--- src/app/components/skills/skill.component.ts
+++ src/app/components/skills/skill.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+export interface Skill {
+  skillName: string;
+  svgPath: string;
+  vendorUrl?: string;
+}
 
 @Component({
   selector: 'app-skill',
@@ -6,4 +12,9 @@ import { Component } from '@angular/core';
   styles: [],
 })
 export class SkillComponent {
+  @Input() skill: Skill = {
+    skillName: 'Angular',
+    svgPath: 'assets/logos/angular.logos',
+    vendorUrl: 'https://www.angular.io',
+  };
 }
--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Skill } from "./skill.component";
 
 @Component({
   selector: 'app-skills',
@@ -6,4 +7,107 @@ import { Component } from '@angular/core';
   styles: [],
 })
 export class SkillsComponent {
+  @Input() skills: Skill[][] = [
+    [
+      {
+        skillName: 'C#',
+        svgPath: 'assets/logos/csharp-original.svg',
+        vendorUrl: 'https://docs.microsoft.com/en-us/dotnet/csharp/',
+      },
+      {
+        skillName: 'Java',
+        svgPath: 'assets/logos/java-icon.svg',
+        vendorUrl: 'https://openjdk.java.net/',
+      },
+      {
+        skillName: 'TypeScript',
+        svgPath: 'assets/logos/typescript-original.svg',
+        vendorUrl: 'https://www.typescriptlang.org/',
+      },
+      {
+        skillName: 'JavaScript',
+        svgPath: 'assets/logos/javascript-original.svg',
+        vendorUrl: 'https://www.javascript.com/',
+      },
+      {
+        skillName: 'Python',
+        svgPath: 'assets/logos/python-icon.svg',
+        vendorUrl: 'https://www.python.org/',
+      },
+      {
+        skillName: 'C++',
+        svgPath: 'assets/logos/cplusplus-original.svg',
+        vendorUrl: 'https://www.cplusplus.com/',
+      },
+    ],
+    [
+      {
+        skillName: '.NET',
+        svgPath: 'assets/logos/dotnet.svg',
+        vendorUrl: 'https://dotnet.microsoft.com/',
+      },
+      {
+        skillName: 'Angular',
+        svgPath: 'assets/logos/angular.svg',
+        vendorUrl: 'https://www.angular.io/',
+      },
+      {
+        skillName: 'React',
+        svgPath: 'assets/logos/react-original.svg',
+        vendorUrl: 'https://reactjs.org/',
+      },
+      {
+        skillName: 'NextJS',
+        svgPath: 'assets/logos/nextjs-original-wordmark.svg',
+        vendorUrl: 'https://nextjs.org/',
+      },
+      {
+        skillName: 'NodeJS',
+        svgPath: 'assets/logos/nodejs-original.svg',
+        vendorUrl: 'https://nodejs.org/',
+      },
+      {
+        skillName: 'MySQL',
+        svgPath: 'assets/logos/mysql-original-wordmark.svg',
+        vendorUrl: 'https://www.mysql.com/',
+      },
+    ],
+    [
+      {
+        skillName: 'Unity',
+        svgPath: 'assets/logos/unity3d-icon.svg',
+        vendorUrl: 'https://unity.com/',
+      },
+      {
+        skillName: 'Tailwind CSS',
+        svgPath: 'assets/logos/tailwindcss.svg',
+        vendorUrl: 'https://tailwindcss.com/',
+      },
+      {
+        skillName: 'Bootstrap',
+        svgPath: 'assets/logos/bootstrap-logo.svg',
+        vendorUrl: 'https://getbootstrap.com/',
+      },
+      {
+        skillName: 'Docker',
+        svgPath: 'assets/logos/docker.png',
+        vendorUrl: 'https://www.docker.com/',
+      },
+      {
+        skillName: 'Figma',
+        svgPath: 'assets/logos/figma-original.svg',
+        vendorUrl: 'https://www.figma.com/',
+      },
+      {
+        skillName: 'Sketch',
+        svgPath: 'assets/logos/sketch-original.svg',
+        vendorUrl: 'https://www.sketch.com/',
+      },
+      {
+        skillName: 'Blender',
+        svgPath: 'assets/logos/blender.png',
+        vendorUrl: 'https://www.blender.org/',
+      },
+    ],
+  ];
 }
