a64a029c26571be351531d42d4317cff651736b4
Brian Lai <brianlai30@outlook.com>
2021-05-02 17:00:26 -0700

    feat: add page not found route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, UrlTree } from '@angular/router';
 import { HomeComponent } from "./pages/home/home.component";
+import { PageNotFoundComponent } from "./pages/page-not-found";
 
 const routes: Routes = [
   {
@@ -8,10 +9,18 @@ const routes: Routes = [
     pathMatch: 'full',
     component: HomeComponent,
   },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    malformedUriErrorHandler: (error, urlSerializer, url) => {
+      return new UrlTree();
+    },
+  })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Meta, Title } from "@angular/platform-browser";
+import { Title } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -11,49 +11,10 @@ export class AppComponent implements OnInit {
 
   constructor(
     private titleService: Title,
-    private metaService: Meta,
   ) {
   }
 
   ngOnInit(): void {
     this.titleService.setTitle(this.title);
-    // this.metaService.addTags([
-    //   {
-    //     name: 'title',
-    //     content: 'Brian Lai title',
-    //   },
-    //   {
-    //     property: 'og:title',
-    //     content: 'Brian Lai og:title',
-    //   },
-    //   {
-    //     name: 'description',
-    //     content: 'description',
-    //   },
-    //   {
-    //     property: 'og:description',
-    //     content: 'og:description',
-    //   },
-    //   {
-    //     property: 'og:type',
-    //     content: 'website',
-    //   },
-    //   {
-    //     property: 'og:url',
-    //     content: 'https://www.bhlai.com/',
-    //   },
-    //   {
-    //     property: 'og:image',
-    //     content: 'assets/favicon.png',
-    //   },
-    //   {
-    //     name: 'theme-color',
-    //     content: '#E11D48',
-    //   },
-    //   {
-    //     name: 'twitter:card',
-    //     content: 'summary_large_image',
-    //   },
-    // ]);
   }
 }
