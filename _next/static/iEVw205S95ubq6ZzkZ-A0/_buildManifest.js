06640b51e82cf209e5c8da33bfd2f1b28d5ccb47
blai30 <brianlai30@outlook.com>
2022-01-15 04:07:06 +0000

    Deploying to gh-pages from @ blai30/blai30.github.io@228dffe55239d8191bfe180f570beab809b29249 🚀

---
+++ _next/static/iEVw205S95ubq6ZzkZ-A0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/614-28a91388f2eb2804.js","static/chunks/pages/index-a5260fa1e6dd1c7c.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
