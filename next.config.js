7372bb83ad6ee62b7bf2ffa8d062e1007b89aa56
Brian Lai <brianlai30@outlook.com>
2025-01-17 16:54:43 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const runtimeCaching = require('next-pwa/cache')
 
 // module.exports = withPWA({
 module.exports = {
+  output: 'export',
   reactStrictMode: true,
   pwa: {
     disable: process.env.NODE_ENV === 'development',
