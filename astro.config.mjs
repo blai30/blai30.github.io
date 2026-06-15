e2240d3632bc3d23f391e83d3a6489741fea65f2
Brian Lai <brianlai30@outlook.com>
2026-06-15 15:48:31 -0700

    Set up GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://blai30.github.io',
+  site: 'https://brianlai.dev',
   integrations: [react(), mdx(), sitemap()],
   markdown: {
     shikiConfig: {
